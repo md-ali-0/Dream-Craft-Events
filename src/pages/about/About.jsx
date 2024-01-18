989c591a8e2f21382ea8c2b3ec71718fef3c7fc9
md-ali-0 <mohammad.98482@gmail.com>
2024-01-18 21:23:01 +0600

    About Us Page Created

---
+++ src/pages/about/About.jsx
@@ -0,0 +1,61 @@
+const About = () => {
+  return (
+    <div>
+      <h2 className="text-4xl text-center mt-5">About Us</h2>
+      <h3 className="text-3xl font-semibold text-center m-5">
+        Dream Craft Events
+      </h3>
+      <div className="mx-5 p-3 space-y-7">
+        <div>
+          <h4 className="text-2xl mb-2">Original⇢</h4>
+          <p className="text-sm">
+            We have the sense fresh, new and original ideas that will ensure the
+            event we organize is successful, unique and memorable. And We are
+            “Dream Craft Events” With our young and creative minds, we always
+            ensure that we bring out outstanding events that are easily
+            distinguished from other events of the same field.
+          </p>
+        </div>
+        <div>
+          <h4 className="text-2xl mb-2">Creative⇢</h4>
+          <p>
+            We are creative in every aspect of the event; planning, promotional
+            and publicity as well as execution. By being creative, our events
+            are more outstanding and will leave a great impact not just to us,
+            but to our clients as well.
+          </p>
+        </div>
+        <div>
+          <h4 className="text-2xl mb-2">Maticulous⇢</h4>
+          <p>
+            We are very particular in every aspect of the event management to
+            guarantee the event we organise runs perfectly and efficiently.
+          </p>
+        </div>
+        <div>
+          <h4 className="text-2xl mb-2">Event Services⇢</h4>
+          <p>
+            With many years of experience in event organising, we understand
+            that there many factors that lead to a well-organised and successful
+            event. These aspects include events sponsorship, event marketing and
+            promotions as well as producing event merchandising. Each of these
+            aspects is managed carefully as they are the main components of the
+            main event.
+          </p>
+        </div>
+        <div>
+          <h4 className="text-2xl mb-2">Satisfaction⇢</h4>
+          <p>
+            Of course, achieving our clients’ satisfaction is what motivates us
+            to bring out the best events. We believe in the strengths of each of
+            our team members and we have proved to exceed our clients’
+            expectations and presenting them with successful events that raved
+            fantastic reviews around the world!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/main";
+import About from "../pages/about/About";
 import Booking from "../pages/bookings/Booking";
 import ErrorPage from "../pages/error/ErrorPage";
 import Home from "../pages/home/Home";
@@ -18,6 +19,10 @@ const Router = createBrowserRouter([
         path: "/booking",
         element: <Booking />,
       },
+      {
+        path: '/about',
+        element: <About/>
+      }
     ],
   },
 ]);
