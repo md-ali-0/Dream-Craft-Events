5fa8d2247eddcc62c61e34348ee03975b252f51b
Nelima sultana <neelimasultana6@gmail.com>
2024-01-25 21:50:28 +0600

    add story in about page

--- src/components/shared/navbar/Navbar.jsx
+++ src/components/shared/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import { useContext, useState } from 'react';
-import { Link, NavLink } from 'react-router-dom';
-import { AuthContext } from '../../../AuthProvider/AuthProvider';
+import { useContext, useState } from "react";
+import { LuMenu, LuSearch } from "react-icons/lu";
+import { RxCross2 } from "react-icons/rx";
+import { Link, NavLink } from "react-router-dom";
+import { AuthContext } from "../../../AuthProvider/AuthProvider";
+import logo from "../../../assets/logo/logo.png";
+import Container from "../../container/Container";
 
 const Navbar = () => {
-
   const { user, logOut } = useContext(AuthContext);
 
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -13,125 +16,149 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="bg-[#FFFFFF] p-4 shadow flex justify-between items-center">
-      <div className="flex items-center">
-        <div className="text-gray-800 font-bold text-xl">
-          <span className='text-blue-600 font-bold text-2xl'>C</span>vent
-        </div>
-        <div className="md:hidden">
-          <button
-            onClick={toggleMenu}
-            className="text-gray-800 focus:outline-none"
-          >
-            <svg
-              className="h-6 w-6 fill-current"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
+    <nav className="bg-[#FFFFFF] py-4 shadow ">
+      <Container>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center">
+            <div className="md:hidden mx-4">
+              <button
+                onClick={toggleMenu}
+                className="text-gray-800  focus:outline-none"
+              >
+                <LuMenu size={30} className="text-dark_01" />
+              </button>
+            </div>
+            <div className="text-gray-800 font-bold text-xl">
+              <Link to="/">
+                <img src={logo} alt="" />
+              </Link>
+            </div>
+
+            <div
+              className={`md:flex gap-6 md:ml-12 list-none ${
+                isMenuOpen
+                  ? "fixed w-full flex flex-col justify-center items-center z-10 mt-96 text-2xl space-y-2 py-96  text-white  bg-black bg-opacity-95"
+                  : "hidden"
+              }`}
             >
-              {isMenuOpen ? (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M2 3C2 2.44772 2.44772 2 3 2H21C21.5523 2 22 2.44772 22 3C22 3.55228 21.5523 4 21 4H3C2.44772 4 2 3.55228 2 3ZM22 9C22 8.44772 21.5523 8 21 8H3C2.44772 8 2 8.44772 2 9C2 9.55228 2.44772 10 3 10H21C21.5523 10 22 9.55228 22 9ZM21 14H3C2.44772 14 2 14.4477 2 15C2 15.5523 2.44772 16 3 16H21C21.5523 16 22 15.5523 22 15C22 14.4477 21.5523 14 21 14ZM21 20H3C2.44772 20 2 20.4477 2 21C2 21.5523 2.44772 22 3 22H21C21.5523 22 22 21.5523 22 21C22 20.4477 21.5523 20 21 20Z"
-                />
-              ) : (
-                <path
-                  fillRule="evenodd"
-                  clipRule="evenodd"
-                  d="M2 4C2 3.44772 2.44772 3 3 3H21C21.5523 3 22 3.44772 22 4C22 4.55228 21.5523 5 21 5H3C2.44772 5 2 4.55228 2 4ZM2 10C2 9.44772 2.44772 9 3 9H21C21.5523 9 22 9.44772 22 10C22 10.5523 21.5523 11 21 11H3C2.44772 11 2 10.5523 2 10ZM2 16C2 15.4477 2.44772 15 3 15H21C21.5523 15 22 15.4477 22 16C22 16.5523 21.5523 17 21 17H3C2.44772 17 2 16.5523 2 16ZM3 21C2.44772 21 2 20.5523 2 20C2 19.4477 2.44772 19 3 19H21C21.5523 19 22 19.4477 22 20C22 20.5523 21.5523 21 21 21H3Z"
-                />
+              {isMenuOpen && (
+                <RxCross2
+                  onClick={toggleMenu}
+                  className="text-3xl cursor-pointer"
+                ></RxCross2>
               )}
-            </svg>
-          </button>
-        </div>
+              <li>
+                <NavLink to="/" className="">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/booking" className="">
+                  Booking
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/About" className="">
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/Services" className="">
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/Contact" className="">
+                  Contact
+                </NavLink>
+              </li>
+            </div>
+          </div>
+          <div className="flex items-center ml-4">
+            <div className="mr-4">
+              {/* Add your search icon component or link here */}
+              <span className="text-gray-800">
+                <LuSearch />
+              </span>
+            </div>
+            <div>
+              {/* Add your login link or button here */}
+              {/* <NavLink to="/login" className="text-gray-800">Login</NavLink> */}
 
-        <div className={`md:flex space-x-4 list-none ml-4 ${isMenuOpen ? 'block' : 'hidden'}`}>
-          <li>
-            <NavLink to="/" className="text-gray-800">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/booking" className="text-gray-800">Booking</NavLink>
-          </li>
-          <li>
-            <NavLink to="/About" className="text-gray-800">About</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Services" className="text-gray-800">Services</NavLink>
-          </li>
-          <li>
-            <NavLink to="/Contact" className="text-gray-800">Contact</NavLink>
-          </li>
-        </div>
-      </div>
-      <div className="flex items-center">
-        <div className="mr-4">
-          {/* Add your search icon component or link here */}
-          <span className="text-gray-800">🔍</span>
-        </div>
-        <div>
-          {/* Add your login link or button here */}
-          {/* <NavLink to="/login" className="text-gray-800">Login</NavLink> */}
+              <div className="">
+                {user?.email ? (
+                  <div className="dropdown dropdown-end">
+                    <div className="flex flex-col items-center gap-2">
+                      <div className="flex items-center gap-2">
+                        <div className=" flex flex-col justify-center">
+                          <p>{user.displayName}</p>
+                        </div>
 
-          <div className="">
-
-            {
-              user?.email ? <div className="dropdown dropdown-end">
-                <div className="flex flex-col items-center gap-2">
-                  <div className="flex items-center gap-2">
-                    <div className=" flex flex-col justify-center">
-                      <p>{user.displayName}</p>
-
-                    </div>
-
-                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-
-                      <div className="w-10 rounded-full">
-                        <img src={user.photoURL} />
+                        <label
+                          tabIndex={0}
+                          className="btn btn-ghost btn-circle avatar"
+                        >
+                          <div className="w-10 rounded-full">
+                            <img src={user.photoURL} />
+                          </div>
+                        </label>
                       </div>
-                    </label>
+
+                      <div>
+                        <button
+                          onClick={logOut}
+                          className="btn btn-sm  btn-primary bg-[#F4E869] text-black"
+                        >
+                          Logout
+                        </button>
+                      </div>
+                    </div>
+                    <ul
+                      tabIndex={0}
+                      className="menu menu-sm dropdown-content mt-3 p-2 shadow rounded-box w-52  bg-[#F4E869] pb-[100px] z-20"
+                    >
+                      <li>
+                        <button className="btn btn-sm  btn-ghost">
+                          {user.displayName}
+                        </button>
+                        <button className="btn btn-sm  btn-ghost">
+                          {user.email}
+                        </button>
+                      </li>
+                      <li>
+                        <button
+                          onClick={logOut}
+                          className="btn btn-sm btn-primary bg-[#F4E869] text-black"
+                        >
+                          Logout
+                        </button>
+                      </li>
+                    </ul>
                   </div>
+                ) : (
+                  <div className="flex gap-4">
+                    <Link
+                      to="/login"
+                      type="button"
+                      className="inline-block rounded bg-primary/10 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary/90 transition duration-150 ease-in-out hover:bg-primary/20 focus:bg-primary/30 focus:outline-none focus:ring-0 active:bg-primary/20"
+                    >
+                      Login
+                    </Link>
 
-                  <div>
-
-                    <button
-                      onClick={logOut}
-                      className="btn btn-sm  btn-primary bg-[#F4E869] text-black">Logout</button>
-
+                    <Link
+                      to="/register"
+                      type="button"
+                      className="mr-3 inline-block rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary/95 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary/90 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary/90"
+                    >
+                      Register
+                    </Link>
                   </div>
-                </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52  bg-[#F4E869] pb-[100px] z-20">
-                  <li>
-                    <button className="btn btn-sm  btn-ghost">{user.displayName}</button>
-                    <button className="btn btn-sm  btn-ghost">{user.email}</button>
-
-                  </li>
-                  <li>
-                    <button
-                      onClick={logOut}
-                      className="btn btn-sm btn-primary bg-[#F4E869] text-black">Logout</button>
-
-                  </li>
-                </ul>
+                )}
               </div>
-                :
-                <div>
-                  <Link to='/login'>
-                    <button className="btn btn-sm  btn-primary bg-[#F4E869] text-black">Login</button>
-                  </Link>
-                  /
-                  <Link to='/register'>
-                    <button className="btn btn-sm text-black btn-primary bg-[#F4E869]">Register</button>
-                  </Link>
-
-                </div>
-
-
-
-
-            }
+            </div>
           </div>
         </div>
-      </div>
+      </Container>
     </nav>
   );
 };
