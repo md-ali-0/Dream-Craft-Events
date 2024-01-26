fffdfd37d7acb0f5ba95fc0216c7f4a0e2fef6ca
NizamChy <nizam.chy.cs@gmail.com>
2024-01-26 12:14:54 +0600

    shop page merged

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -5,6 +5,11 @@ import Booking from "../pages/bookings/Booking";
 import ErrorPage from "../pages/error/ErrorPage";
 import Home from "../pages/home/Home";
 import Contact from "../pages/contact/Contact";
+import EventDetails from "../pages/eventDetails/EventDetails";
+import Login from "../pages/auth/login/Login";
+import Register from "../pages/auth/register/Register";
+import Shop from "../pages/shop/Shop";
+
 
 const Router = createBrowserRouter([
   {
@@ -34,7 +39,16 @@ const Router = createBrowserRouter([
       },
       {
         path: "/register",
-        element: <RegisterPage></RegisterPage>
+        element: <Register/>
+      },
+      {
+        path: '/event-details',
+        element: <EventDetails/>
+      },
+
+      {
+        path: "/shop",
+        element: <Shop></Shop>
       }
      
     ],
