e3698941ad07f35429bb151c1c09813d54f65542
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2024-02-09 21:09:01 +0600

    payment page design done

--- src/components/cards/PricingCards.jsx
+++ src/components/cards/PricingCards.jsx
@@ -4,13 +4,13 @@ import vip from "../../assets/Pricing/vip.png";
 import Container from "../container/Container";
 import SectionTitle from "../sectionTitle/SectionTitle";
 
-const PricingCards = ({ticketPriceHandle, ticketPriceHandle2}) => {
+const PricingCards = ({ ticketPriceHandle, ticketPriceHandle2 }) => {
     return (
         <section id="price">
             <Container>
                 <div className="mt-3.5">
                     <SectionTitle heading={"Get Your Seat"} subHeading={'Ticket Plan'}></SectionTitle>
-                    
+
                 </div>
 
                 <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
@@ -23,11 +23,11 @@ const PricingCards = ({ticketPriceHandle, ticketPriceHandle2}) => {
                             <p className='py-3 px-6 line-through'>Video presentations</p>
                             <p className='border-y-2 py-3 px-6 line-through'>Meet all of our event speakers</p>
                         </div>
-                        <button className='py-3 px-6 border-2 border-b-4 border-r-4 border-black'>
-                            Get Ticket
+                        <button className='py-3 hover:border-rose-600 px-6 border-2 border-b-4 border-r-4 border-black'>
+                            Select
                         </button>
                     </div>
-                  
+
                     <div onClick={ticketPriceHandle} className='border-2 hover:shadow-2xl px-8 my-6 py-8 flex flex-col justify-center items-center space-y-2'>
                         <img className='w-20' src={vip} alt="" />
                         <p className='text-gray-500 italic'>VIP ticket</p>
@@ -37,8 +37,8 @@ const PricingCards = ({ticketPriceHandle, ticketPriceHandle2}) => {
                             <p className='py-3 px-6'>Video presentations</p>
                             <p className='border-y-2 py-3 px-6'>Meet all of our event speakers</p>
                         </div>
-                        <button className='py-3 px-6 border-2 border-b-4 border-r-4 border-black'>
-                            Get Ticket
+                        <button className='py-3 px-6 hover:border-rose-600 border-2 border-b-4 border-r-4 border-black'>
+                            Select
                         </button>
                     </div>
                 </div>
