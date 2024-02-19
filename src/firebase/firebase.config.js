44d6e36c04086cd2278167d169e5b70109852220
md-ali-0 <mohammad.98482@gmail.com>
2024-02-19 18:04:48 +0600

    create event api added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp, } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQysagE3aGiM39xem9iU-59w3lIuQ8sfs",
+  authDomain: "dream-craft-events-da8fb.firebaseapp.com",
+  projectId: "dream-craft-events-da8fb",
+  storageBucket: "dream-craft-events-da8fb.appspot.com",
+  messagingSenderId: "923847074423",
+  appId: "1:923847074423:web:6cc8329fa7464c9a751c72"
+};
+
+const app = initializeApp(firebaseConfig);
+export const storage = getStorage(app);
+
+export default app
---
+++ src/utils/useImageUpload.js
@@ -0,0 +1,22 @@
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
+import { v4 as uuidv4 } from 'uuid';
+import { storage } from '../firebase/firebase.config';
+
+const uploadImage = async (file) => {
+    try {
+
+        if (!file || !file?.name) {
+            return
+        }
+        const storageRef = ref(storage, `images/${file.name + uuidv4()}`);
+
+        const fileRef = await uploadBytes(storageRef, file);
+        const imageUrl = await getDownloadURL(fileRef.ref);
+        return imageUrl;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export default uploadImage;
