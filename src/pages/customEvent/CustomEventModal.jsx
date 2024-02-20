691c17420b5c9db8f1eca24020362a7b08706374
Rakibul Hasan <87831331+rhakib@users.noreply.github.com>
2024-02-21 01:49:18 +0600

    modal design done for custom event page

---
+++ src/pages/customEvent/CustomEventModal.jsx
@@ -0,0 +1,18 @@
+import React, { Fragment } from 'react';
+
+const CustomEventModal = ({ isVisible, children }) => {
+
+    if (!isVisible) {
+        return null;
+    }   
+
+    return (
+
+        <Fragment>
+            {children}
+        </Fragment>
+
+    );
+};
+
+export default CustomEventModal;
