56558cf33b632ffa8832fc9435e4cbf76f6bb432
Saikat <sachinsingha7899@gmail.com>
2024-02-25 18:19:48 +0600

    do some chage in chatbot

--- src/chatbotComponents/Chatbot/BackendAPI.js
+++ src/chatbotComponents/Chatbot/BackendAPI.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class BackendAPI {
   async fetchEvents() {
     try {
-      const response = await axios.get('https://dream-craft-server.vercel.app/events_limit5');
+      const response = await axios.get('https://dream-craft-server.vercel.app/events');
       return response.data;
     } catch (error) {
       console.error('Error fetching events:', error);
