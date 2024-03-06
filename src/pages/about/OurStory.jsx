8d2c45928c9dbfd29ebabd5edd0086a539b4c1d8
NizamChy <nizam.chy.cs@gmail.com>
2024-03-06 12:08:51 +0600

    instant review functionality added

--- src/pages/about/OurStory.jsx
+++ src/pages/about/OurStory.jsx
@@ -1,18 +1,24 @@
-import React from "react";
+
+import Container from "../../components/container/Container";
 
 const OurStory = () => {
   return (
-    <div className="text-center font-serif p-10  ">
+    <Container>
+    <div className="text-center font-serif p-10  mt-10">
       <h2 className="text-3xl font-semibold mb-5">Our Story</h2>
-      <p className="text-xl">
+      {/* <p className="text-xl">
         We are an Exclusive ,independent BD(Bangladesh ) based Event planners .
         We offer premium wedding service, Birthday party, Perfect party organizing & perfect wedding for the bride and
         groom, and their family with a touch of personalization . We provide
         stylish ,exclusive and creative Events in Bangladesh . Award winning BD
         Event Management & Wedding Planners offer a highly creative ,friendly &
         Professional service to Bangladesh  LOVE TO PLAN
+      </p> */}
+      <p className="text-xl ">
+      We are an exclusive, independent event planning company based in Bangladesh, specializing in premium wedding services, birthday parties, and perfect party organizing tailored to the needs of our clients. With a touch of personalization, we create stylish, exclusive, and creative events throughout Bangladesh. Our award winning team of event management and wedding planners offers a highly creative, friendly, and professional service dedicated to making every event memorable. At Bangladesh Love to Plan, we are passionate about bringing dreams to life.
       </p>
     </div>
+    </Container>
   );
 };
 
